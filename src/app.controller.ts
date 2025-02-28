0e2afc11b8fbaf7fc17ac78dfaf91ae809c84b08
David Santana <davidsantana06@hotmail.com>
2025-02-28 16:32:03 -0300

    feat: add resume export route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,23 @@
-import { Controller, Get, Param, Render } from '@nestjs/common';
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { Controller, Get, Param, Query, Render, Res } from '@nestjs/common';
+import {
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
+import { Response } from 'express';
+import { ViewService } from './view/view.service';
 import { ProfileService } from './profile/profile.service';
 
 @ApiTags('app')
+@ApiResponse({ status: 404, description: 'Profile not found' })
 @Controller()
 export class AppController {
-  constructor(private readonly profileService: ProfileService) {}
+  constructor(
+    private readonly profileService: ProfileService,
+    private readonly viewService: ViewService,
+  ) {}
 
   @ApiOperation({
     summary: 'Render a resume',
@@ -28,13 +40,72 @@ export class AppController {
       },
     },
   })
-  @ApiResponse({ status: 404, description: 'Profile not found' })
-  @Render('resume')
+  @Render('resume-v1')
   @Get(':profileHandle')
   async renderResume(@Param('profileHandle') profileHandle: string) {
     const profile = await this.profileService.getUnique({
       handle: profileHandle,
     });
     return { ...profile };
+  }
+
+  @ApiOperation({
+    summary: 'Export a resume',
+    description:
+      'Exports the resume for the given profile handle as PDF or PNG file',
+  })
+  @ApiParam({
+    name: 'profileHandle',
+    type: 'string',
+    description: 'The profile handle',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'Success',
+    content: {
+      'application/pdf': {
+        schema: {
+          type: 'string',
+          format: 'binary',
+          example: 'JVBERi0xLjcKCjEgMCBvYmogICUgZW50cnk...',
+        },
+      },
+      'application/png': {
+        schema: {
+          type: 'string',
+          format: 'binary',
+          example: 'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAY...',
+        },
+      },
+    },
+  })
+  @ApiQuery({
+    name: 'format',
+    enum: ['pdf', 'png'],
+    description: 'The file format',
+    required: true,
+  })
+  @Get(':profileHandle/export')
+  async exportResume(
+    @Param('profileHandle') profileHandle: string,
+    @Query('format') format: 'pdf' | 'png',
+    @Res() res: Response,
+  ) {
+    const { name, ...rest } = await this.profileService.getUnique({
+      handle: profileHandle,
+    });
+
+    const buffer = await this.viewService.export(
+      'resume-v2',
+      { name, ...rest },
+      format,
+    );
+    const filename = `${encodeURIComponent(name)}.${format}`;
+
+    res.set({
+      'Content-Type': `application/${format}`,
+      'Content-Disposition': `attachment; filename*=UTF-8''${filename}`,
+    });
+    res.send(buffer);
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { ProfileModule } from './profile/profile.module';
 import { UserModule } from './user/user.module';
+import { ViewModule } from './view/view.module';
 import { AppController } from './app.controller';
 
 @Module({
@@ -11,6 +12,7 @@ import { AppController } from './app.controller';
     ConfigModule.forRoot({ isGlobal: true }),
     ProfileModule,
     UserModule,
+    ViewModule,
   ],
   controllers: [AppController],
 })
