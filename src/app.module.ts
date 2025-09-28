80d20ea384fbd6224f8b924410fb0bdc97879bf7
David Santana <davidsantana06@hotmail.com>
2025-09-28 17:55:11 -0300

    refactor: replace common module with swagger

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,13 @@
 import { Module } from '@nestjs/common';
-import { CommonModule } from './common/common.module';
 import AuthModule from './auth/auth.module';
 import ProfileModule from './profile/profile.module';
+import SwaggerModule from './swagger/swagger.module';
 import UserModule from './user/user.module';
 import ViewModule from './view/view.module';
 import AppController from './app.controller';
 
 @Module({
-  imports: [
-    AuthModule,
-    ProfileModule,
-    UserModule,
-    ViewModule,
-    CommonModule,
-  ],
+  imports: [AuthModule, ProfileModule, SwaggerModule, UserModule, ViewModule],
   controllers: [AppController],
 })
 export default class AppModule {}
